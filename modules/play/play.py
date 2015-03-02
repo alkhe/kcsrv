082078f8f23e3b5b5bc6a9e8e91cbda49c1e21a0
edge <sort@programmer.net>
2015-03-02 23:37:01 +0900

    redirect to login when play requested when anonymous

--- modules/play/play.py
+++ modules/play/play.py
@@ -1,9 +1,12 @@
-from flask import Blueprint, request, url_for, render_template
+from flask import Blueprint, request, url_for, render_template, redirect
 from flask.ext.security import current_user
 
 play = Blueprint('play', __name__, template_folder='templates')
 
 @play.route('/')
 def index():
-	api_token = current_user.api_token
-	return render_template('play/index.html', api_token=api_token)
+	if hasattr(current_user, 'api_token'):
+		api_token = current_user.api_token
+		return render_template('play/index.html', api_token=api_token)
+	else:
+		return redirect('/account/login')
